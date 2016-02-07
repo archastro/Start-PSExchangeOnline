8985af859c4f6d3378c7a032fda05e0466941ca2
archastro <archastro@gmail.com>
2016-02-07 22:47:53 +1100

    Initial commit

---
+++ start-psexchangeonline.ps1
@@ -0,0 +1,3 @@
+$UserCredential = Get-Credential
+$Office365 = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Office365
